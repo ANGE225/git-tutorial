48d1dff31a7a4127981c1772c5c635b975782edd
EC2 Default User <ec2-user@ip-172-31-31-0.ec2.internal>
2021-02-25 06:37:03 +0000

    file update

--- edureka.py
+++ edureka.py
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
 Ange is the best in the country and in the word . 
 =======
-Ange is the best in the country.I love america  and Africa
+Ange is the best in the country.I love america  and Africa and Asia
 >>>>>>> ef37b12761ce6f47c4774bd4f50c17b3d0e105b4
